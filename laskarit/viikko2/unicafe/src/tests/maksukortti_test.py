fae872b4a10ca18497631506c4fc8fb973d3b4f0
Nael Shaban <nael.shaban@gmail.com>
2021-11-13 13:53:11 +0200

    add tests for unicafe/src/maksukortti.py

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,25 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(5)
+        self.assertEqual(self.maksukortti.saldo, 15)
+
+    def test_saldo_vahenee_oikein_jos_rahaa_on_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(self.maksukortti.saldo, 5)
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(20)
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_palautusarvo_oikein_jos_rahat_riittavat(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(5), True)
+
+    def test_palautusarvo_oikein_jos_rahat_ei_riitta(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(20), False)
+
