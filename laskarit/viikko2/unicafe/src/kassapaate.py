80a334af3d2624dc292303fdef2f683c4393b1d2
Nael Shaban <nael.shaban@gmail.com>
2021-11-13 16:40:34 +0200

    Refactored uncicafe/src/kassapaate.py

--- laskarit/viikko2/unicafe/src/kassapaate.py
+++ laskarit/viikko2/unicafe/src/kassapaate.py
@@ -4,37 +4,38 @@ class Kassapaate:
         self.edulliset = 0
         self.maukkaat = 0
 
-    def syo_edullisesti_kateisella(self, maksu):
-        if maksu >= 240:
-            self.kassassa_rahaa = self.kassassa_rahaa + 240
+    def kasvata_myyntilukua(self, hinta):
+        if hinta == 240:
             self.edulliset += 1
-            return maksu - 240
+        if hinta == 400:
+            self.maukkaat += 1
+
+    def syo_kateisella(self, maksu, hinta):
+        if maksu >= hinta:
+            self.kassassa_rahaa = self.kassassa_rahaa + hinta
+            self.kasvata_myyntilukua(hinta)
+            return maksu - hinta
         else:
             return maksu
+
+    def syo_edullisesti_kateisella(self, maksu):
+        return self.syo_kateisella(maksu, 240)
 
     def syo_maukkaasti_kateisella(self, maksu):
-        if maksu >= 400:
-            self.kassassa_rahaa = self.kassassa_rahaa + 400
-            self.maukkaat += 1
-            return maksu - 400
-        else:
-            return maksu
+        return self.syo_kateisella(maksu, 400)
+
+    def syo_kortilla(self, kortti, hinta):
+        if kortti.saldo >= hinta:
+            kortti.ota_rahaa(hinta)
+            self.kasvata_myyntilukua(hinta)
+            return True
+        return False
 
     def syo_edullisesti_kortilla(self, kortti):
-        if kortti.saldo >= 240:
-            kortti.ota_rahaa(240)
-            self.edulliset += 1
-            return True
-        else:
-            return False
+        return self.syo_kortilla(kortti, 240)
 
     def syo_maukkaasti_kortilla(self, kortti):
-        if kortti.saldo >= 400:
-            kortti.ota_rahaa(400)
-            self.maukkaat += 1
-            return True
-        else:
-            return False
+        return self.syo_kortilla(kortti, 400)
 
     def lataa_rahaa_kortille(self, kortti, summa):
         if summa >= 0:
