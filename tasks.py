f907c140b8fe4a8a5df9c820faad5b011dffcad3
Nael Shaban <nael.shaban@gmail.com>
2021-11-30 16:06:54 +0200

    Add invoke task for linting

--- tasks.py
+++ tasks.py
@@ -17,5 +17,9 @@ def coverage_report(ctx):
     ctx.run("coverage html")
 
 @task
+def lint(ctx):
+    ctx.run("pylint src")
+
+@task
 def format(ctx):
     ctx.run("autopep8 --in-place --recursive src")
